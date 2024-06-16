b8abe4d5404d730936accdbb0dba4ead66a84f63
SK Rohan Parveag <parveagr@gmail.com>
2024-06-16 15:38:15 +0000

    Sync LeetCode submission Runtime - 35 ms (2.76%), Memory - 11.8 MB (100.00%)

---
+++ solutions/0330-patching-array/solution.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def minPatches(self, nums, n):
+        """
+        :type nums: List[int]
+        :type n: int
+        :rtype: int
+        """
+        patches = 0
+        miss = 1
+        i = 0
+        while miss <= n:
+            if i < len(nums) and nums[i] <= miss:
+                miss += nums[i]
+                i += 1
+            else:
+                patches += 1
+                miss += miss
+        return patches
+        
