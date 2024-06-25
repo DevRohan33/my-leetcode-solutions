de0086f9233536a136bff41318d69f42cd986205
SK Rohan Parveag <parveagr@gmail.com>
2024-06-25 15:38:20 +0000

    Sync LeetCode submission Runtime - 11 ms (89.20%), Memory - 11.6 MB (100.00%)

---
+++ solutions/1894-merge-strings-alternately/solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        merged = ""
+        i = 0
+        j = 0
+        while i < len(word1) or j < len(word2):
+            if i < len(word1):
+                merged += word1[i]
+                i += 1
+            if j < len(word2):
+                merged += word2[j]
+                j += 1
+        return merged
