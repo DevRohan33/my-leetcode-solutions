6c1eb01eafeed7e6a697024d69db092aafd787b1
SK Rohan Parveag <parveagr@gmail.com>
2024-07-02 17:30:36 +0000

    Sync LeetCode submission Runtime - 545 ms (99.98%), Memory - 0.0B (100.00%)

---
+++ solutions/2087-confirmation-rate/solution.sql
@@ -0,0 +1,25 @@
+# Write your MySQL query statement below
+WITH ConfirmationsSummary AS (
+    SELECT 
+        user_id,
+        COUNT(*) AS total_requests,
+        SUM(CASE WHEN action = 'confirmed' THEN 1 ELSE 0 END) AS confirmed_requests
+    FROM 
+        Confirmations
+    GROUP BY 
+        user_id
+)
+
+SELECT 
+    s.user_id,
+    ROUND(COALESCE(cs.confirmed_requests, 0) / COALESCE(cs.total_requests, 1), 2) AS confirmation_rate
+FROM 
+    Signups s
+LEFT JOIN 
+    ConfirmationsSummary cs
+ON 
+    s.user_id = cs.user_id
+ORDER BY 
+    s.user_id;
+
+
