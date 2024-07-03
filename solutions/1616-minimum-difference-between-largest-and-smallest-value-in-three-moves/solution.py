2fa130003e0c0577e552e293e133c89676dc870f
SK Rohan Parveag <parveagr@gmail.com>
2024-07-03 16:21:34 +0000

    Sync LeetCode submission Runtime - 275 ms (5.26%), Memory - 20.2 MB (0.00%)

---
+++ solutions/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def minDifference(self, nums):
+        if len(nums) <= 4:
+            return 0
+
+        nums.sort()
+        n = len(nums)
+
+        
+        return min(nums[n-1] - nums[3],
+                   nums[n-2] - nums[2],
+                   nums[n-3] - nums[1],  
+                   nums[n-4] - nums[0])
+solution = Solution()
+        
