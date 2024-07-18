64e8be8686662d82b4a02ce60d0c673934e46aac
SK Rohan Parveag <parveagr@gmail.com>
2024-07-18 04:03:52 +0000

    Sync LeetCode submission Runtime - 18 ms (5.15%), Memory - 11.8 MB (100.00%)

---
+++ solutions/1319-unique-number-of-occurrences/solution.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def uniqueOccurrences(self, arr):
+        count = {}
+        for num in arr:
+            if num in count:
+                count[num] += 1
+            else:
+                count[num] = 1
+        return len(count.values()) == len(set(count.values()))
