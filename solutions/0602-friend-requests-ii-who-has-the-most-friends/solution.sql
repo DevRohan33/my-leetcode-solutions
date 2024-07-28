1c984b483ae886f1512b478128b46e6964710243
SK Rohan Parveag <parveagr@gmail.com>
2024-07-28 17:13:06 +0000

    Sync LeetCode submission Runtime - 367 ms (32.86%), Memory - 0.0B (100.00%)

--- solutions/0602-friend-requests-ii-who-has-the-most-friends/solution.sql
+++ solutions/0602-friend-requests-ii-who-has-the-most-friends/solution.sql
@@ -1,21 +1,19 @@
 # Write your MySQL query statement below
-WITH FriendCounts AS (
-    SELECT requester_id AS person_id, COUNT(DISTINCT accepter_id) AS friend_count
+WITH FriendCount AS (
+    SELECT requester_id AS id
     FROM RequestAccepted
-    GROUP BY requester_id
     UNION ALL
-    SELECT accepter_id AS person_id, COUNT(DISTINCT requester_id) AS friend_count
+    SELECT accepter_id AS id
     FROM RequestAccepted
-    GROUP BY accepter_id
 ),
-AggregatedCounts AS (
-    SELECT person_id, SUM(friend_count) AS total_friends
-    FROM FriendCounts
-    GROUP BY person_id
+FriendSummary AS (
+    SELECT id, COUNT(*) AS num
+    FROM FriendCount
+    GROUP BY id
 )
-SELECT person_id AS id, total_friends AS num
-FROM AggregatedCounts
-ORDER BY total_friends DESC
+SELECT id, num
+FROM FriendSummary
+ORDER BY num DESC
 LIMIT 1;
 
 
